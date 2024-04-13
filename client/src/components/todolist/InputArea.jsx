9fcf6e68747ffc34e3a9a0deee352d90792325d6
petauristini <petauristini@proton.me>
2024-04-13 20:06:59 +0200

    Add hover indicators

--- client/src/components/todolist/InputArea.jsx
+++ client/src/components/todolist/InputArea.jsx
@@ -2,24 +2,30 @@ import React, { useState } from "react";
 
 function InputArea(props) {
   const [inputText, setInputText] = useState("");
+  const [hover, setHover] = useState(false);
 
   function handleChange(event) {
     const newValue = event.target.value;
     setInputText(newValue);
   }
 
-  function handleClick() {
+  function handleSubmit(event) {
+    event.preventDefault();
     props.addItem(inputText);
     setInputText("");
   }
 
   return (
-    <div className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={handleClick}>
+      <button
+        onMouseOver={() => setHover(true)}
+        onMouseOut={() => setHover(false)}
+        style={hover ? { backgroundColor: "#5c0a8f" } : {}}
+      >
         <span>Add</span>
       </button>
-    </div>
+    </form>
   );
 }
 
--- client/src/components/todolist/ToDoItem.jsx
+++ client/src/components/todolist/ToDoItem.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToDoItem(props) {
+  const [crossed, setCrossed] = useState(false);
+
   return (
     <div
       onClick={() => {
         props.onChecked(props.id);
       }}
+      onMouseOver={() => setCrossed(true)}
+      onMouseOut={() => setCrossed(false)}
+      style={
+        crossed
+          ? {
+              textDecoration: "line-through",
+              textDecorationColor: "#B95CF4",
+              textDecorationThickness: "3px",
+            }
+          : null
+      }
     >
       <li>{props.text}</li>
     </div>
